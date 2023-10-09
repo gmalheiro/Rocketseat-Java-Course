aadace299e760ad8d1660d17118e724304f2e926
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-09 18:04:00 -0300

    Added method to controller to say hello world

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/controllers/MyFirstController.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/controllers/MyFirstController.java
@@ -1,5 +1,16 @@
 package br.com.gabrielmalheiro.todolist.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firstRoute")
 public class MyFirstController {
-    
+
+    @GetMapping("/firstMethod")
+    public String FirstMessage(){
+        return "Hello World! Java";
+    }
+
 }
