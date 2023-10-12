398de4c5c07272243c0a714675744312ba890366
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-11 22:40:42 -0300

    Added lombok annotations

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
@@ -1,38 +1,14 @@
 package br.com.gabrielmalheiro.todolist.users;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+
 public class UserModel {
 
     private String username;
     private String name;
     private String password;
-
-    @Override
-    public String toString() {
-        return (this.username + "," + this.name + "," + this.password);
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
 }
