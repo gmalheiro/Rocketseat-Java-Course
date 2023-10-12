42277de9dccd5044c8890fc3d89af2d2dae260e7
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-11 22:21:19 -0300

    Added Getters and setters

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
@@ -2,12 +2,37 @@ package br.com.gabrielmalheiro.todolist.users;
 
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
 
-    @Override 
-    public String toString(){
-        return (this.username + "," + this.name + "," + this.password       );
+    @Override
+    public String toString() {
+        return (this.username + "," + this.name + "," + this.password);
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
