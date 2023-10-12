89689f43f1844b6f493b27a3c9490c83716df9f2
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-12 12:39:28 -0300

    Changed create method return type to ResponseEntity

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserController.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserController.java
@@ -1,6 +1,8 @@
 package br.com.gabrielmalheiro.todolist.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,15 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/createUser")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
         var user = this.userRepository.findByUsername(userModel.getUsername());
         
         if(user != null){
-            System.out.println("User exists!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User Exists");
         }
                 
         var userCreated = this.userRepository.save(userModel);
 
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
