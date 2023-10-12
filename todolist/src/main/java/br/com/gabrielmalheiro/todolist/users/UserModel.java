f59dee84636c19a104f22603c8923e8439d1f2ea
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-11 22:43:01 -0300

    Added comments

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/users/UserModel.java
@@ -7,7 +7,9 @@ import lombok.ToString;
 @ToString
 
 public class UserModel {
-
+    /* If you want a prop to have a specific accessor like the name prop should only be readable and should not have a setter 
+     * You can add the annotation like this:
+      @Getter */
     private String username;
     private String name;
     private String password;
