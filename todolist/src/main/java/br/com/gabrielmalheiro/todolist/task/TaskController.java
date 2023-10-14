0e7ee888e71dcde30842c1a3e29ed7ccfb4e0ae6
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-14 15:54:46 -0300

    Get by id method done

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/task/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,4 +64,11 @@ public class TaskController {
 
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+
+    @GetMapping("/getTaskById/{id}")
+    public ResponseEntity<TaskModel> get(@PathVariable UUID id){
+        var task = this.taskRepository.findById(id).orElse(new TaskModel());
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
 }
