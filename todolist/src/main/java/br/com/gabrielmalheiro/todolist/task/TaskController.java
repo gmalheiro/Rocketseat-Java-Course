28d4380e9e730e393ee8201ba8489e81dd52d4ef
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-13 22:54:16 -0300

    Added date validation

--- todolist/src/main/java/br/com/gabrielmalheiro/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/gabrielmalheiro/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.gabrielmalheiro.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,20 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/createTask")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
-        System.out.println("In task controller");
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        
         var userId = request.getAttribute("idUser");
         taskModel.setUserId((UUID) userId);
+                
+        System.out.println("In task controller");
+        
+        var currentDate = LocalDateTime.now();
+        
+        if(currentDate.isAfter(taskModel.getStartAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start date must be greater than the actual date");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
